1503564f6b2e7a6604fead712e4ee787a00e5264
Sxela <defiler.off@gmail.com>
2018-11-20 18:50:59 +0300

    Add files via upload
    now calculating value of each connection by adding up values of duplicate addresses

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,6 @@ let web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/"
 
 let addresses = new Array<string>();
 
-class account_entry 
-{
-    account:string;
-    to: Array<string>;
-    from: Array<string>;
-}
 
 //read a block and add all addresses to an array, with check for duplicates
 async function get_addresses(i, addresses)
@@ -31,30 +25,32 @@ async function get_addresses(i, addresses)
     else {console.log("block #" + i +" is empty")};
 }
 
-//scan block and add connections to a given address
-async function get_connections(i, address, to, from)
+//scan block and add connections to a given address, storing value of any connection
+async function get_connections(i, address, to_arr, from_arr)
 {
-    const count = await web3.eth.getBlockTransactionCount(i);
-    if (count != 0)
-    {
+   
         const blk = await web3.eth.getBlock(i,1);
-        blk.transactions.forEach(item => 
+        blk.transactions.forEach(tx => 
                 {
-                    if(item.to == address)
+                    if(tx.to == address) //if desired address is found as recipient
                     {
                     //check address array for duplicates before adding
-                    var index = from.findIndex(row => row === item.from);
-                    if (index == -1 ) from.push(item.from);
+                    var index = from_arr.findIndex(row => row === tx.from);
+                    if (index == -1 ) from_arr.push([tx.from, tx.value/1000000000000000000]) //if address is not found in the array - add address + tx value
+                    else //if address found - add value 
+                        from_arr[index][1]+=from_arr.value;
                     }
-                    if(item.from == address)
+
+                    if(tx.from == address) //if desired address is found as sender
                     {
-                    var index = to.findIndex(row => row === item.to);
-                    if (index == -1 ) to.push(item.to);
+                    var index = to_arr.findIndex(row => row === tx.to);
+                    if (index == -1 ) to_arr.push([tx.to, tx.value/1000000000000000000])
+                    else //if address found - add value 
+                        from_arr[index][1]+=to_arr.value;
                     }
                 })
        
-    }
-    else {console.log("block #" + i +" is empty")};
+    
 }
 
 //read a range of blocks
@@ -67,11 +63,24 @@ async function readblock(startblock, endblock, array)
     console.log(addresses);
 
 }
+
+//tuple test
+type entry = [string, number];
+let test:entry;
+test=['test',1];
+console.log(test);
+let to = new Array<entry>();
+to.push(['test',1]);
+to.push(['test',2]);
+to[1][1]+=2;
+console.log(to);
+
+
 //search a range of blocks for an address
 async function search_address(startblock, endblock, address)
 {
-    let to = new Array<string>();
-    let from = new Array<string>();
+    let to = new Array<entry>();
+    let from = new Array<entry>();
     for (let i=startblock;i<=endblock;i++)
     {
         if (i % 10 == 0) console.log('checking block #'+i);
@@ -86,4 +95,4 @@ async function search_address(startblock, endblock, address)
 }
 
 //readblock(1728879,1728879+0,addresses);
-search_address(6735100,6735155,'0x876EabF441B2EE5B5b0554Fd502a8E0600950cFa');
+search_address(6740125,6740125+1,'0x876EabF441B2EE5B5b0554Fd502a8E0600950cFa');
