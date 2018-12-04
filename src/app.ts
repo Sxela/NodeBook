7e5c8e820c89a1bfe4587ef99665698f6708a2a1
Mac <user@MacBook-Pro-Mac-2.local>
2018-12-04 15:09:30 +0300

    Made a working aggregate() model. Cleaned app.ts

--- src/app.ts
+++ src/app.ts
@@ -49,149 +49,9 @@ async function addBlocks(startblock, endblock) //add blocks + transactions
     
 }
 
-async function tx_get_connections(address) //get connections to a given address from tx db
-{
-let date = Date();
-let to = new Array<entry>();
-let from = new Array<entry>();
-
-await eth_Tx.find({'transaction.from': address}, async (err, txs2)=>{
-    await txs2.forEach( element2 => {
-        var index = to.findIndex(row => row[0] === element2.transaction.to);
-                            if (index == -1 ) {to.push([element2.transaction.to, element2.transaction.value/1000000000000000000, 1])}
-                            else //if address found - add value and tx count
-                            {
-                            to[index][1]+=element2.transaction.value/1000000000000000000;
-                            to[index][2]+=1;
-                            }   
-                     
-    });
-});
-
-await eth_Tx.find({'transaction.to': address}, async (err, txs)=>{
-    await txs.forEach( element => {
-       var index = from.findIndex(row => row[0] === element.transaction.from); //if address is not found in the array - add address + tx value
-                       if (index == -1 ) { from.push([element.transaction.from, element.transaction.value/1000000000000000000, 1])} 
-                       else //if address found - add value and tx count
-                       {
-                           from[index][1]+=element.transaction.value/1000000000000000000;
-                           from[index][2]+=1;
-                       } 
-    });
-});
-console.log('\naddress ' + address +'\n\nhas sent to \n');
-console.log( to);
-console.log('\n\nand has received from \n');
-console.log( from);
-
-    
-console.log(date)
-console.log(Date());
-return {"to":to,"from":from};
-}
-
-export async function tx_get_connections_out(address) //get connections to a given address from tx db
-{
-let to = new Array<entry>();
-
-await eth_Tx.find({'transaction.from': address}, 'transaction.to transaction.value', async (err, txs2)=>{
-     txs2.forEach( element2 => {
-        var index = to.findIndex(row => row[0] === element2.transaction.to);
-                            if (index == -1 ) {to.push([element2.transaction.to, element2.transaction.value/1000000000000000000, 1])}
-                            else //if address found - add value and tx count
-                            {
-                            to[index][1]+=element2.transaction.value/1000000000000000000;
-                            to[index][2]+=1;
-                            }   
-                     
-    });
-});
-return to;
-}
-
-export async function tx_get_connections_in(address) //get connections to a given address from tx db
-{
-let from = new Array<entry>();
-
-await eth_Tx.find({'transaction.to': address}, 'transaction.from transaction.value', async (err, txs)=>{
-    txs.forEach( element => {
-       var index = from.findIndex(row => row[0] === element.transaction.from); //if address is not found in the array - add address + tx value
-                       if (index == -1 ) { from.push([element.transaction.from, element.transaction.value/1000000000000000000, 1])} 
-                       else //if address found - add value and tx count
-                       {
-                           from[index][1]+=element.transaction.value/1000000000000000000;
-                           from[index][2]+=1;
-                       } 
-    });
-});
-return from;
-}
-
-
 //test polygon
 //0xb854bf62681303653c461Efa5Cd8E7555230E628
 //0xEA674fdDe714fd979de3EdF0F56AA9716B898ec8 ethermint
-
-async function test (address) {
-await eth_Tx.find({'transaction.from': address},  async (err, txs)=>{
-    //console.log(txs.length);
-    //console.log(await tx_get_connections_in(0xEA674fdDe714fd979de3EdF0F56AA9716B898ec8));
-}
-).limit(100);
-
-};
-
-test(0xEA674fdDe714fd979de3EdF0F56AA9716B898ec8);
-eth_Tx.find({'transaction.from': '0xEA674fdDe714fd979de3EdF0F56AA9716B898ec8'},  (err, txs)=>{
-    //console.log(txs.length);
-}
-).limit(100);
-
-eth_Tx.find({'transaction.from': '0xb854bf62681303653c461Efa5Cd8E7555230E628'}, 'transactions.to transactions.value').count().exec((err,count)=>
-{
-    let to = new Array<entry>();
-    let batchsize = 1000; //taking results in batches;
-    for (let i = 0;i<=count;i+=batchsize)
-    {
-        eth_Tx.find({'transaction.from': '0xb854bf62681303653c461Efa5Cd8E7555230E628'}).skip(i).limit(batchsize).exec((err,res)=>
-        {
-
-            res.forEach( element2 => {
-                var index = to.findIndex(row => row[0] === element2.transaction.to);
-                                    if (index == -1 ) {to.push([element2.transaction.to, element2.transaction.value/1000000000000000000, 1])}
-                                    else //if address found - add value and tx count
-                                    {
-                                    to[index][1]+=element2.transaction.value/1000000000000000000;
-                                    to[index][2]+=1;
-                                    }   
-                             
-            });
-            //console.log(res.length + ' from batch '+i);
-            //console.log(to);
-        });
-    }
-    //console.log('count '+count)
-    return to;
-})
-
-
-eth_Tx.aggregate([
-    {
-        $match: 
-        {'transaction.from': '0xEA674fdDe714fd979de3EdF0F56AA9716B898ec8'}
-    },
-    {
-        $group:
-        { 
-            _id: "$transaction.to",
-            Txes: { $sum: 1 },
-            value: { $sum : "$transaction.value"}            
-        }
-    }
-]).exec((err,res)=>{
-//console.log(res);
-return res;
-})
 
 async function convert_tx_value()
 {
@@ -213,14 +73,11 @@ let batchsize = 100; //taking results in batches;
     }
 }
 
-//convert_tx_value();
-//console.log(1)
-//db_block.getBlock(6000000);
 //addBlocks(6000000,6010000);
 
-export async function tx_get_connections_out_aggr(address) //get connections from a given address from tx db
+export async function tx_get_connections_out(address) //get connections from a given address from tx db
 {
-    let aggr = await eth_Tx.aggregate([
+    return await eth_Tx.aggregate([
         {
             $match: 
             {'transaction.from': address}
@@ -233,16 +90,14 @@ export async function tx_get_connections_out_aggr(address) //get connections fro
                 value: { $sum : "$transaction.value"}            
             }
         }
-    ]).limit(20).exec(async (err,res)=>{
-        console.log(res);
-    })
-    return aggr;
+    ])
+    .sort({value: 'desc'})
+    .limit(20)
 }
 
-export async function tx_get_connections_in_aggr(address) //get connections to a given address from tx db
+export async function tx_get_connections_in(address) //get connections to a given address from tx db
 {
-    let aggr;
-    await eth_Tx.aggregate([
+    return await eth_Tx.aggregate([
         {
             $match: 
             {'transaction.to': address}
@@ -255,6 +110,7 @@ export async function tx_get_connections_in_aggr(address) //get connections to a
                 value: { $sum : "$transaction.value"}            
             }
         }
-    ]).limit(20).exec((err,res)=>{
-    })
+    ])
+    .sort({value: 'desc'})
+    .limit(20)
 }
--- src/routes/addresses.ts
+++ src/routes/addresses.ts
@@ -9,23 +9,15 @@ router.get('/', async (req, res) => {
 })
 
 router.get('/addresses/', async (req, res) => {
-    res.send({
-        tx_in: await app.tx_get_connections_in('0xfE03e5d2aDBddDa0B6F4c92a21c916442D629591'),
-        tx_out: await app.tx_get_connections_out('0xfE03e5d2aDBddDa0B6F4c92a21c916442D629591'),
-    })
+    res.send(
+        'Please specify an address!'
+    )
 })
 
 router.get('/addresses/:id', async (req, res) => {
     res.send({
         tx_in: await app.tx_get_connections_in(req.params.id),
         tx_out: await app.tx_get_connections_out(req.params.id),
-    })
-})
-
-router.get('/add/:id', async (req, res) => {
-    res.send({
-        tx_in: await app.tx_get_connections_in_aggr(req.params.id),
-        tx_out: await app.tx_get_connections_out_aggr(req.params.id),
     })
 })
 
