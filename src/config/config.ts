716520a8dc496adbacf045498768a11dd1485ee1
Sxela <defiler.off@gmail.com>
2019-01-14 21:12:10 +0300

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,5 +4,5 @@ module.exports = {
     //mongoUrl: 'docker.for.win.localhost:27017', //for localhost mongo
     mongoDatabase: 'test_block',
     mongoOptions: '{ useNewUrlParser: true }',
-    web3Provider: "https://mainnet.infura.io/v3/25c667224edd406e857d23c6e4c07bc4"
+    web3Provider: "https://mainnet.infura.io/"
 }
