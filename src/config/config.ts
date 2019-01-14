46efd7494879dbac8e4d270d4e709670739557b0
Sxela <defiler.off@gmail.com>
2019-01-14 21:11:41 +0300

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 module.exports = {
     port: 8081,
-    //mongoUrl: 'data:27017', //for docker mongo
-    mongoUrl: 'docker.for.win.localhost:27017', //for localhost mongo
+    mongoUrl: 'data:27017', //for docker mongo
+    //mongoUrl: 'docker.for.win.localhost:27017', //for localhost mongo
     mongoDatabase: 'test_block',
     mongoOptions: '{ useNewUrlParser: true }',
     web3Provider: "https://mainnet.infura.io/v3/25c667224edd406e857d23c6e4c07bc4"
